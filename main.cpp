3cc60e83b6d4fabc9a4e5273227586c638e670c7
Tati650 <tatiponce06@gmail.com>
2025-11-10 21:11:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <pqxx/pqxx>
+
 using namespace std;
+
 int main() {
     try {
         cout << "SISTEMA ESCOLAR - PRUEBA DE CONEXION" << endl;
         cout << "========================================" << endl;
 
-
-        string connection_string =
-            "dbname=programacion "
+        // Configuración de conexión
+        const string connection_string =
+            "dbname=escuela "
             "user=postgres "
             "password=1234 "
             "host=localhost "
@@ -19,34 +21,55 @@ int main() {
         // Crear conexión
         pqxx::connection conn(connection_string);
 
-        if (conn.is_open()) {
-            cout << "Conexion exitosa a PostgreSQL" << endl;
-            cout << "   Base de datos: " << conn.dbname() << endl;
-        } else {
+        if (!conn.is_open()) {
             cout << "No se pudo conectar a la base de datos" << endl;
             return 1;
         }
 
-        // Probar una consulta simple
+        cout << "Conexion exitosa a PostgreSQL" << endl;
+        cout << "Base de datos: " << conn.dbname() << endl;
+
+        // Consulta de prueba
         cout << "\nEjecutando consulta de prueba..." << endl;
         pqxx::work txn(conn);
-        pqxx::result result = txn.exec("SELECT version(), current_timestamp;");
+        pqxx::result result = txn.exec("SELECT version(), current_timestamp");
         txn.commit();
 
-        cout << " Resultado obtenido:" << endl;
+        cout << "Resultado obtenido:" << endl;
         for (const auto& row : result) {
-            cout << "   PostgreSQL: " << row[0].c_str() << endl;
-            cout << "   Fecha/Hora: " << row[1].c_str() << endl;
+            cout << "PostgreSQL: " << row[0].c_str() << endl;
+            cout << "Fecha/Hora: " << row[1].c_str() << endl;
         }
 
         cout << "\nTodo funciono correctamente" << endl;
 
+        cout << "----------Menu----------" << endl;
+        cout << "Presione: (1), (2), (3) o (4)" << endl;
+        cout << "(1)Ver alumnos" << endl;
+        cout << "(2)gestionar alumnos" << endl;
+        cout << "(3)gestionar notas" << endl;
+        cout << "(4)gestionar asistencias" << endl;
+        cout << "------------------------" << endl;
+        cin >> numero;
+        if (numero == 1) {
+
+        }else
+            if (numero == 2) {
+
+        }else
+            if (numero == 3) {
+
+        }else
+            if (numero == 4) {
+
+        }else cout << "Intente otra vez." << endl;
     } catch (const exception& e) {
-        cerr << " Error: " << e.what() << endl;
+        cerr << "Error: " << e.what() << endl;
         return 1;
     }
 
     cout << "\nPresiona Enter para salir...";
     cin.get();
     return 0;
+
 }
